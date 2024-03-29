68ee797cd7a68235982df2c8d0a55d0db4f26664
ZYH-0419 <yuhongzhu9@gmail.com>
2024-03-29 10:36:14 +0800

    added in robust sd

--- hypothesis 3.R
+++ hypothesis 3.R
@@ -1,5 +1,6 @@
 library(lmtest)
 library(ggplot2)
+library("sandwich") # for vcovHC
 
 # Create a linear regression model with host_listings_count as the independent variable
 model3 <- lm(review_scores_rating ~ host_listings_count, data = airbnb)
@@ -10,21 +11,18 @@ summary(model3)
 # Check for homoscedasticity with the Breusch-Pagan test
 bptest(model3)
 
-model3log <- lm(log(review_scores_rating) ~ host_listings_count, data = airbnb)
-summary(model3log)
-
-# Homoscedasticity
-test_result <- bptest(model3log)
-print(test_result)
-
 # Check for linearity 
-#plot(airbnb$host_listings_count, airbnb$review_scores_rating, main = "Listing count vs Review Scores", xlab = "Host Listing Count", ylab = "Review Scores Rating")
-#abline(model3, col = "red")
-
-# Homoscedasticity
-#plot(model3log$fitted.values, resid(model3log), main = "Homoscedasticity Check: Fitted vs Residuals", xlab = "Fitted values", ylab = "Residuals")
-#abline(h = 0, col = "red") 
+plot(airbnb$host_listings_count, airbnb$review_scores_rating, main = "Listing count vs Review Scores", xlab = "Host Listing Count", ylab = "Review Scores Rating")
+abline(model3, col = "red")
 
 # after adding controls
-model3_new<- lm(log(review_scores_rating) ~ host_listings_count + host_response_rate+ host_response_time+ host_acceptance_rate+ host_is_superhost, data = airbnb)
+model3_new<- lm(review_scores_rating ~ host_listings_count + host_response_rate+ host_response_time+ host_acceptance_rate+ host_is_superhost, data = airbnb)
 summary(model3_new)
+bptest(model3_new)
+# estimating heteroscedasticity-robust standard errors
+robust_se <- coeftest(model3_new, vcov = vcovHC(model3_new))
+
+# Displaying the result with robust standard errors
+print(robust_se)
+
+
